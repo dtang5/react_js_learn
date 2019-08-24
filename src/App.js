1daaf404998fdf59e0fb667fafed063bf67b1997
Daniel Tang <dt19@rice.edu>
2019-08-24 09:24:49 -0500

    Running checklist

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos'
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the Trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with Wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with Boss',
+        completed: true
+      }
+    ]
+  }
+  markComplete = (id) => {
+    this.setState({ todos : this.state.todos.map(todo => {
+      if (todo.id == id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+}
+
+export default App;
